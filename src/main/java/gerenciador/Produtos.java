cd93a354f6c69b8a364b445e72806403e66f3f7c
adriano <adrianobinan@gmail.com>
2026-04-23 08:42:06 -0300

    feat: paginas principais e classe produto

---
+++ src/main/java/gerenciador/Produtos.java
@@ -0,0 +1,48 @@
+package gerenciador;
+
+public class Produtos {
+	 
+	private int id;
+	private String nome;
+	private int unidadeCompra;
+	private String descricao;
+	private int qtdPrevistoMes;
+	private double precoMaxComprado;
+	
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int unidadeCompra() {
+		return unidadeCompra;
+	}
+	public void unidadeCompra(int uCompra) {
+		this.unidadeCompra = uCompra;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQtdPrevistoMes() {
+		return qtdPrevistoMes;
+	}
+	public void setQtdPrevistoMes(int qtdPrevisto) {
+		this.qtdPrevistoMes = qtdPrevisto;
+	}
+	public double getPrecoMaxComprado() {
+		return precoMaxComprado;
+	}
+	public void setPrecoMaxComprado(double precoMax) {
+		this.precoMaxComprado = precoMax;
+	} 
+}
